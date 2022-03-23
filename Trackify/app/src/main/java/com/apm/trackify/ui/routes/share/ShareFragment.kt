af4869b9f5808855501c3f04348b93cd3b93886b
Laura Ben Artiles <lbenartiles@gmail.com>
2022-03-23 09:59:29 +0100

    added action add playlist to route

--- Trackify/app/src/main/java/com/apm/trackify/ui/routes/share/ShareFragment.kt
+++ Trackify/app/src/main/java/com/apm/trackify/ui/routes/share/ShareFragment.kt
@@ -1,4 +1,4 @@
-package com.apm.trackify.ui.routes.search
+package com.apm.trackify.ui.routes.share
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,11 +10,12 @@ import androidx.navigation.Navigation
 import com.apm.trackify.R
 import com.apm.trackify.databinding.PlaylistsLandingFragmentBinding
 import com.apm.trackify.databinding.RoutesSearchFragmentBinding
+import com.apm.trackify.databinding.RoutesShareFragmentBinding
 
-class SearchFragment : Fragment() {
+class ShareFragment : Fragment() {
 
     private lateinit var navc: NavController
-    private var _binding: RoutesSearchFragmentBinding? = null
+    private var _binding: RoutesShareFragmentBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,18 +27,9 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = RoutesSearchFragmentBinding.inflate(inflater, container, false)
+        _binding = RoutesShareFragmentBinding.inflate(inflater, container, false)
         val root: View = binding.root
         return root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        navc = Navigation.findNavController(view)
-        binding.btnAddPlaylist2route.setOnClickListener {
-            navc.navigate(R.id.action_routes_search_to_route_share)
-        }
     }
 
     override fun onDestroyView() {
