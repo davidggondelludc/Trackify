3299ab13449a4418b77f2e53f2845d7c7a70d883
Diego <diego.ramil.lopez@gmail.com>
2022-03-31 11:47:54 +0200

    Upload unstaged changes

---
+++ Trackify/app/src/main/java/com/apm/trackify/ui/playlists/create/PlaylistCreateFragment.kt
@@ -0,0 +1,49 @@
+package com.apm.trackify.ui.playlists.create
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.apm.trackify.R
+import com.apm.trackify.databinding.PlaylistCreateFragmentBinding
+import com.apm.trackify.util.extension.toast
+
+class PlaylistCreateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = PlaylistCreateFragmentBinding.inflate(inflater, container, false).root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val binding = PlaylistCreateFragmentBinding.bind(view)
+
+        setupToolbar(binding.toolbar)
+
+        binding.search.setOnClickListener { it.context.toast("Showing results") }
+        binding.save.setOnClickListener {
+            val navController = it.findNavController()
+            navController.navigateUp()
+        }
+    }
+
+    private fun setupToolbar(toolbar: Toolbar) {
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.playlists_fragment,
+                R.id.routes_fragment,
+                R.id.profile_fragment
+            )
+        )
+
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+}
