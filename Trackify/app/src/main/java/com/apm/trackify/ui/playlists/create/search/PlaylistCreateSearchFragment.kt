055715db7fd4c2e9ae4cc3614d4e1802510fea9e
Hilda <h.rvelo@udc.es>
2022-04-05 14:08:59 +0200

    List of searching results has been filled with items.

--- Trackify/app/src/main/java/com/apm/trackify/ui/playlists/create/search/PlaylistCreateSearchFragment.kt
+++ Trackify/app/src/main/java/com/apm/trackify/ui/playlists/create/search/PlaylistCreateSearchFragment.kt
@@ -6,13 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.ConcatAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.apm.trackify.R
 import com.apm.trackify.databinding.PlaylistsCreateSearchFragmentBinding
+import com.apm.trackify.ui.playlists.tracks.PlaylistTracksViewModel
+import com.apm.trackify.ui.playlists.tracks.adapter.AddTrackAdapter
 
 class PlaylistCreateSearchFragment : Fragment() {
+    private val viewModel: PlaylistTracksViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,6 +31,8 @@ class PlaylistCreateSearchFragment : Fragment() {
         val binding = PlaylistsCreateSearchFragmentBinding.bind(view)
 
         setupToolbar(binding.toolbar)
+
+        setupRecyclerView(binding.rvSearchedSongs)
     }
 
     private fun setupToolbar(toolbar: Toolbar) {
@@ -36,5 +46,20 @@ class PlaylistCreateSearchFragment : Fragment() {
         )
 
         toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+
+        val addTrackAdapter = AddTrackAdapter()
+
+        viewModel.getTracks().observe(viewLifecycleOwner) {
+            addTrackAdapter.submitList(it)
+        }
+
+        val concatAdapter = ConcatAdapter()
+        concatAdapter.addAdapter(addTrackAdapter)
+
+        recyclerView.adapter = concatAdapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
     }
 }
